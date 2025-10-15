eca7e3398683d9c75701189f4b6e64f2007e32d5
michxlmg <jhefrigasparalmonacid0@gmail.com>
2025-10-15 21:26:15 +0200

    feat: add and update page sections

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -4,3 +4,4 @@ export { default as Logo } from "./logo/index.astro";
 export { default as FAQSection } from "./faq_section/index.astro";
 export { default as Footer } from "./footer/index.astro";
 export { default as CTASection } from "./cta_section/index.astro";
+export { default as StatsSection } from "./stats_section/index.astro";
